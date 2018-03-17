0e93ffc988250dac3777a4ae2fa88fa04f2ecffe
Ikiru Yoshizaki <cherishacquire@outlook.com>
2018-03-17 18:33:21 +0900

    master push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,15 +15,15 @@ GIT_REPO="git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.g
 git submodule init
 git submodule update
 
-remote=`git ls-remote --heads 2> /dev/null | grep publish || true`
+remote=`git ls-remote --heads 2> /dev/null | grep master || true`
 
 if [ -n "$remote" ]; then
-  git clone -b publish "${GIT_REPO}" public
+  git clone -b master "${GIT_REPO}" public
   rm -rf public/*
 else
   git init public
   cd public
-  git checkout -b publish
+  git checkout -b master
   git remote add origin "${GIT_REPO}"
   cd ..
 fi
@@ -34,4 +34,4 @@ git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 git add --all
 git commit -m 'Update [ci skip]'
-git push -f origin publish
+git push -f origin master
